eaf6c262f4400acf9543ff4cffcf32343b8a428e
ndido98 <ndido98@gmail.com>
2021-04-26 11:44:57 +0200

    Add sample class

---
+++ src/main/java/ClassA.java
@@ -0,0 +1,16 @@
+public class ClassA {
+
+    private int value = 0;
+
+    public int get() {
+        return value;
+    }
+
+    public void increment() {
+        value++;
+    }
+
+    public void decrement() {
+        value--;
+    }
+}

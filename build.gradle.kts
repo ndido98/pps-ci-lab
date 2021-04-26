ae0a36762cdc8eee59ccf6dd7a12fb3272b3c483
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2021-04-26 12:27:10 +0200

    Add repository and scala library

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,15 @@
 plugins {
     java
     application
+    scala
+}
+
+repositories {
+    mavenCentral()
 }
 
 dependencies {
+    implementation("org.scala-lang:scala-library:2.13.5")
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.7.1")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.7.1")
 }

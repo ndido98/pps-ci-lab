66b4d4191d7ce5543a332680c311b7465d4e5ab6
Giacomo Cavalieri <giacomo.cavalieri2@studio.unibo.it>
2021-04-26 12:41:19 +0200

    Add Scala library dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,6 +9,7 @@ repositories {
 }
 
 dependencies {
+    implementation("org.scala-lang:scala-library:2.12.2")
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.7.1")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.7.1")
 }
